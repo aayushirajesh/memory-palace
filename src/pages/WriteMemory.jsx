8a69dd08d3293e87c9b3cd0fa61dc563a756a4ec
Rick13211 <rick13211roy@gmail.com>
2026-07-21 07:54:31 +0530

    Replaced alert() in with a custom alert box

--- src/pages/WriteMemory.jsx
+++ src/pages/WriteMemory.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import {MemoryEditor} from "../components/MemoryEditor";
 import { createMemory } from "../services/memoryService";
 
 export default function WriteMemory() {
   const navigate = useNavigate();
+  const [error, setError] = useState(null)
   async function handleSave(title, content, imageBase64, mood) {
     try { 
       await createMemory({
@@ -16,7 +17,10 @@ export default function WriteMemory() {
       navigate("/memorywall"); 
     } catch (error) { 
       console.error(error); 
-      alert(error.message);
+      setError(error.message);
+      setTimeout(()=>{
+        setError(null);
+      }, 3000);
     }
   }
 
@@ -26,6 +30,11 @@ export default function WriteMemory() {
 
   return (
     <div className="flex-1 w-full flex flex-col justify-center py-6 md:py-12 px-4 relative z-10 select-none">
+      {error && (
+          <div className="fixed top-1 z-50 animate-in fade-in slide-in-from-top duration-300 w-full flex justify-center px-6">
+            <Card errorMsg={error} />
+          </div>
+        )}
       <MemoryEditor
         onSave={handleSave}
         onCancel={handleCancel}
